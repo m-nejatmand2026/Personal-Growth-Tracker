8d49d43b3447f013a6408de54fbf0b881a58e5fd
m-nejatmand2026 <m.nejatmand@gmail.com>
2026-08-12 17:27:29 +0200

    Add frontend Plans module manifest

---
+++ public/js/modules/plans/module.js
@@ -0,0 +1,18 @@
+import { bindBudgetPanel, budgetPanelHtml, loadBudgetModel } from '../../features/plan/budgets.js';
+
+export const plansModule = Object.freeze({
+  id: 'plans',
+  contractVersion: 1,
+  dependsOn: ['goals'],
+  defaultEnabled: true,
+  slots: Object.freeze([{ name: 'plan', order: 40 }]),
+  async load({ date }) {
+    return loadBudgetModel(date);
+  },
+  render({ model, models }) {
+    return budgetPanelHtml(model, models.goals?.goals || []);
+  },
+  bind({ model, models, reload }) {
+    bindBudgetPanel(model, models.goals?.goals || [], { reloadPlatform: reload });
+  }
+});
