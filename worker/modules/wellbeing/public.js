3d04d68f21c4c00bee5f4f8e09d31d8b13ffee31
m-nejatmand2026 <m.nejatmand@gmail.com>
2026-08-13 20:30:08 +0200

    Add Wellbeing public contract

--- worker/modules/wellbeing/public.js
+++ worker/modules/wellbeing/public.js
@@ -1,1 +1,46 @@
-export const wellbeingPublicContractVersion = 1;
+import {
+  exportWellbeingData,
+  getDayContextObservation,
+  getEnergyObservation,
+  getSleepObservation,
+  listEnergyObservations,
+  upsertEnergyObservation
+} from './data.js';
+
+function freezeObservation(item) {
+  return item ? Object.freeze({ ...item }) : null;
+}
+
+export const wellbeingContractV1 = Object.freeze({
+  async getDay(DB, profileId, date) {
+    const [energy, sleep, context] = await Promise.all([
+      getEnergyObservation(DB, profileId, date),
+      getSleepObservation(DB, profileId, date),
+      getDayContextObservation(DB, profileId, date)
+    ]);
+
+    return Object.freeze({
+      date,
+      energy: freezeObservation(energy),
+      sleep: freezeObservation(sleep),
+      context: freezeObservation(context)
+    });
+  },
+
+  async getEnergy(DB, profileId, date) {
+    return freezeObservation(await getEnergyObservation(DB, profileId, date));
+  },
+
+  async listEnergy(DB, profileId, options = {}) {
+    const items = await listEnergyObservations(DB, profileId, options);
+    return Object.freeze(items.map((item) => freezeObservation(item)));
+  },
+
+  async recordEnergy(DB, profileId, input) {
+    return freezeObservation(await upsertEnergyObservation(DB, profileId, input));
+  }
+});
+
+export async function exportWellbeingV1(DB, profileId) {
+  return exportWellbeingData(DB, profileId);
+}
