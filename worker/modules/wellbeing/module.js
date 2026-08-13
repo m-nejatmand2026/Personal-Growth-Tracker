906b722c1573c54e60caf1d65300db331f0237c5
m-nejatmand2026 <m.nejatmand@gmail.com>
2026-08-13 20:31:51 +0200

    Register Wellbeing table ownership

--- worker/modules/wellbeing/module.js
+++ worker/modules/wellbeing/module.js
@@ -1,1 +1,15 @@
-export const wellbeingModuleVersion = 1;
+export const wellbeingModule = Object.freeze({
+  id: 'wellbeing',
+  contractVersion: 1,
+  dependsOn: Object.freeze([]),
+  defaultEnabled: true,
+  ownsTables: Object.freeze([
+    'energy_logs_v1',
+    'sleep_logs_v1',
+    'day_context_logs_v1'
+  ]),
+  compatibilityTables: Object.freeze(['energy_logs']),
+  routes: Object.freeze([]),
+  publishes: Object.freeze(['wellbeing.energy-recorded']),
+  subscribes: Object.freeze([])
+});
