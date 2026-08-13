36192dd9b0736e8055360586562ca02d3d13044e
m-nejatmand2026 <m.nejatmand@gmail.com>
2026-08-13 20:42:01 +0200

    Attach Insights module UI

--- public/js/modules/insights/manifest.js
+++ public/js/modules/insights/manifest.js
@@ -1,1 +1,12 @@
-export const insightsModule = Object.freeze({ id: 'insights', contractVersion: 1, dependsOn: ['progress','wellbeing'], defaultEnabled: true, slots: Object.freeze([]) });
+import { renderInsights } from './ui.js';
+
+export const insightsModule = Object.freeze({
+  id: 'insights',
+  contractVersion: 1,
+  dependsOn: ['progress', 'wellbeing'],
+  defaultEnabled: true,
+  slots: Object.freeze([]),
+  render() {
+    return renderInsights();
+  }
+});
