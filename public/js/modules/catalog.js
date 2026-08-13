299d1d3d5bb926874feb30d42ad0aaa29f9f596f
m-nejatmand2026 <m.nejatmand@gmail.com>
2026-08-13 05:47:55 +0200

    Compose Today plan frontend module

--- public/js/modules/catalog.js
+++ public/js/modules/catalog.js
@@ -2,11 +2,13 @@ import { areasModule } from './areas/module.js';
 import { capacityModule } from './capacity/module.js';
 import { goalsModule } from './goals/module.js';
 import { plansModule } from './plans/module.js';
+import { todayIntentionsModule } from './today-intentions/module.js';
 
 // Frontend composition root. Platform/core code must not import business modules.
 export const frontendModules = Object.freeze([
   areasModule,
   goalsModule,
   plansModule,
-  capacityModule
+  capacityModule,
+  todayIntentionsModule
 ]);
