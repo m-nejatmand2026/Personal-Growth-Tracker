b7bc318001c24cc85424706e08a4fdc1953e4b14
m-nejatmand2026 <m.nejatmand@gmail.com>
2026-08-17 16:37:15 +0200

    Cache Experience 2 install guidance assets

--- public/experience/2/sw.js
+++ public/experience/2/sw.js
@@ -1,4 +1,4 @@
-const CACHE='growth-compass-preview2-e2-v28';
+const CACHE='growth-compass-preview2-e2-v29';
 const CORE=[
   '/experience/2/',
   '/experience/2/css/foundation.css',
@@ -15,8 +15,10 @@ const CORE=[
   '/experience/2/css/journal.css',
   '/experience/2/css/wellness.css',
   '/experience/2/css/settings.css',
+  '/experience/2/css/install-app.css',
   '/experience/2/js/theme-bootstrap.js',
   '/experience/2/js/modal-accessibility.js',
+  '/experience/2/js/install-app.js',
   '/experience/2/js/app.js',
   '/experience/2/js/core/api.js',
   '/experience/2/js/core/preferences.js',
