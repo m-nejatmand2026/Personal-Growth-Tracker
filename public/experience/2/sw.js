88bdf0d17e3e4633ca3ff562a47b2db07c4cb67f
m-nejatmand2026 <m.nejatmand@gmail.com>
2026-08-17 15:19:31 +0200

    Cache pre-paint Experience 2 theme bootstrap

--- public/experience/2/sw.js
+++ public/experience/2/sw.js
@@ -1,4 +1,4 @@
-const CACHE='growth-compass-preview2-e2-v22';
+const CACHE='growth-compass-preview2-e2-v23';
 const CORE=[
   '/experience/2/',
   '/experience/2/css/foundation.css',
@@ -15,6 +15,7 @@ const CORE=[
   '/experience/2/css/journal.css',
   '/experience/2/css/wellness.css',
   '/experience/2/css/settings.css',
+  '/experience/2/js/theme-bootstrap.js',
   '/experience/2/js/app.js',
   '/experience/2/js/core/api.js',
   '/experience/2/js/core/preferences.js',
