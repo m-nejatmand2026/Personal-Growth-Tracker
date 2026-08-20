8d47dc13323a08ba7ccc95c5801f798aeb950130
m-nejatmand2026 <m.nejatmand@gmail.com>
2026-08-20 07:52:48 +0200

    Bump Experience 2 cache for palette themes

--- public/experience/2/sw.js
+++ public/experience/2/sw.js
@@ -1,4 +1,4 @@
-const CACHE='growth-compass-preview2-e2-v65';
+const CACHE='growth-compass-preview2-e2-v66';
 const CORE=[
   '/experience/2/',
   '/experience/2/css/foundation.css',
@@ -19,6 +19,7 @@ const CORE=[
   '/experience/2/css/install-app.css',
   '/experience/2/css/liquid.css',
   '/experience/2/css/liquid-polish.css',
+  '/experience/2/css/palettes.css',
   '/experience/2/js/theme-bootstrap.js',
   '/experience/2/js/modal-accessibility.js',
   '/experience/2/js/install-app.js',
