a9e103aec14da13d92e7c46f95ada45b36c3fffc
m-nejatmand2026 <m.nejatmand@gmail.com>
2026-08-22 11:24:51 +0200

    Name the Goals route Direction throughout Experience 2

--- public/experience/2/js/views/goals.js
+++ public/experience/2/js/views/goals.js
@@ -1,3 +1,6 @@
 const DIRECTION_STYLESHEET='/experience/2/css/direction.css';
-if(typeof document!=='undefined'&&!document.querySelector(`link[href="${DIRECTION_STYLESHEET}"]`)){const link=document.createElement('link');link.rel='stylesheet';link.href=DIRECTION_STYLESHEET;link.dataset.experience2Direction='true';document.head.append(link);}
+if(typeof document!=='undefined'){
+  if(!document.querySelector(`link[href="${DIRECTION_STYLESHEET}"]`)){const link=document.createElement('link');link.rel='stylesheet';link.href=DIRECTION_STYLESHEET;link.dataset.experience2Direction='true';document.head.append(link);}
+  document.querySelectorAll('[data-view="goals"] span').forEach(node=>node.textContent='Direction');
+}
 export { loadGoals, renderGoals, bindGoals } from './direction.js';
