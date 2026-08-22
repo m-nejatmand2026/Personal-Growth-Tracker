704ca0ddd8446d4a9bfee751f4a3a11bf1add522
m-nejatmand2026 <m.nejatmand@gmail.com>
2026-08-22 11:16:58 +0200

    Load isolated Direction presentation layer

--- public/experience/2/js/views/goals.js
+++ public/experience/2/js/views/goals.js
@@ -1,1 +1,3 @@
+const DIRECTION_STYLESHEET='/experience/2/css/direction.css';
+if(typeof document!=='undefined'&&!document.querySelector(`link[href="${DIRECTION_STYLESHEET}"]`)){const link=document.createElement('link');link.rel='stylesheet';link.href=DIRECTION_STYLESHEET;link.dataset.experience2Direction='true';document.head.append(link);}
 export { loadGoals, renderGoals, bindGoals } from './direction.js';
