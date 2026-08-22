292a8d0e26e4879822bfc486d9621ea27688425c
m-nejatmand2026 <m.nejatmand@gmail.com>
2026-08-22 11:19:55 +0200

    Keep Direction archive sequencing regression coverage

--- tests/experience2-goal-archive-sequencing.test.js
+++ tests/experience2-goal-archive-sequencing.test.js
@@ -2,9 +2,9 @@ import test from 'node:test';
 import assert from 'node:assert/strict';
 import { readFile } from 'node:fs/promises';
 
-const goals=await readFile(new URL('../public/experience/2/js/views/goals.js',import.meta.url),'utf8');
+const goals=await readFile(new URL('../public/experience/2/js/views/direction.js',import.meta.url),'utf8');
 
-test('Goal archive confirmation releases modal UI before awaiting the network mutation',()=>{
+test('Direction archive confirmation releases modal UI before awaiting the network mutation',()=>{
   const handler=goals.slice(goals.indexOf("host.querySelector('[data-goal-archive-confirm]')"));
   const close=handler.indexOf('close();');
   const archive=handler.indexOf('await goalsCapability.archive(goal.id)');
