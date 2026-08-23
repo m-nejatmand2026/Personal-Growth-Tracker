b7b93026bcc5dde6b581634de006a4364eb275dc
m-nejatmand2026 <m.nejatmand@gmail.com>
2026-08-23 23:00:53 +0200

    Register activity restore and remove routes

--- worker/modules/activities/module.js
+++ worker/modules/activities/module.js
@@ -2,6 +2,8 @@ import {
   archiveActivityRoute,
   createActivityRoute,
   listActivitiesRoute,
+  removeActivityRoute,
+  restoreActivityRoute,
   updateActivityRoute
 } from './routes.js';
 
@@ -16,8 +18,10 @@ export const activitiesModule = Object.freeze({
     { method: 'GET', pattern: '/api/v1/activities', handler: listActivitiesRoute },
     { method: 'POST', pattern: '/api/v1/activities', handler: createActivityRoute },
     { method: 'PUT', pattern: /^\/api\/v1\/activities\/\d+$/, handler: updateActivityRoute },
+    { method: 'POST', pattern: /^\/api\/v1\/activities\/\d+\/restore$/, handler: restoreActivityRoute },
+    { method: 'DELETE', pattern: /^\/api\/v1\/activities\/\d+\/permanent$/, handler: removeActivityRoute },
     { method: 'DELETE', pattern: /^\/api\/v1\/activities\/\d+$/, handler: archiveActivityRoute }
   ]),
-  publishes: Object.freeze(['activity.created', 'activity.updated', 'activity.archived']),
+  publishes: Object.freeze(['activity.created', 'activity.updated', 'activity.archived', 'activity.restored', 'activity.removed']),
   subscribes: Object.freeze([])
 });
