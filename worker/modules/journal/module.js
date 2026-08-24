eab8b2714d9e9036afec94ed952d9d926d054f41
m-nejatmand2026 <m.nejatmand@gmail.com>
2026-08-24 06:34:27 +0200

    Register Journal lifecycle routes

--- worker/modules/journal/module.js
+++ worker/modules/journal/module.js
@@ -1,7 +1,9 @@
 import {
+  archiveJournalRoute,
   createJournalRoute,
   deleteJournalRoute,
   listJournalRoute,
+  restoreJournalRoute,
   updateJournalRoute
 } from './routes.js';
 
@@ -15,9 +17,11 @@ export const journalModule = Object.freeze({
   routes: Object.freeze([
     { method: 'GET', pattern: '/api/v1/journal', handler: listJournalRoute },
     { method: 'POST', pattern: '/api/v1/journal', handler: createJournalRoute },
+    { method: 'POST', pattern: /^\/api\/v1\/journal\/\d+\/restore$/, handler: restoreJournalRoute },
     { method: 'PUT', pattern: /^\/api\/v1\/journal\/\d+$/, handler: updateJournalRoute },
-    { method: 'DELETE', pattern: /^\/api\/v1\/journal\/\d+$/, handler: deleteJournalRoute }
+    { method: 'DELETE', pattern: /^\/api\/v1\/journal\/\d+$/, handler: archiveJournalRoute },
+    { method: 'DELETE', pattern: /^\/api\/v1\/journal\/\d+\/permanent$/, handler: deleteJournalRoute }
   ]),
-  publishes: Object.freeze(['journal.entry-created','journal.entry-updated','journal.entry-deleted']),
+  publishes: Object.freeze(['journal.entry-created','journal.entry-updated','journal.entry-archived','journal.entry-restored','journal.entry-deleted']),
   subscribes: Object.freeze([])
 });
