63e46900eddf4d8250e632fc5a0555bf34c5d473
m-nejatmand2026 <m.nejatmand@gmail.com>
2026-08-26 15:42:38 +0200

    Support bounded Journal transport

--- public/experience/2/js/capabilities/journal.js
+++ public/experience/2/js/capabilities/journal.js
@@ -1,7 +1,7 @@
 import { api } from '../core/api.js';
 
 export const journalCapability = Object.freeze({
-  list({ q = '', from = '', to = '', limit = 80, includeArchived = false, archivedOnly = false } = {}) {
+  list({ q = '', from = '', to = '', limit = 80, includeArchived = false, archivedOnly = false } = {}, options = {}) {
     const query = new URLSearchParams();
     if (q) query.set('q', q);
     if (from) query.set('from', from);
@@ -9,11 +9,11 @@ export const journalCapability = Object.freeze({
     query.set('limit', String(limit));
     if (includeArchived) query.set('include_archived', '1');
     if (archivedOnly) query.set('archived_only', '1');
-    return api.get(`/v1/journal?${query}`);
+    return api.get(`/v1/journal?${query}`, options);
   },
   create(input) { return api.post('/v1/journal', input); },
   update(id, input) { return api.put(`/v1/journal/${id}`, input); },
   archive(id) { return api.delete(`/v1/journal/${id}`); },
   restore(id) { return api.post(`/v1/journal/${id}/restore`, {}); },
-  remove(id) { return api.delete(`/v1/journal/${id}/permanent`); }
+  remove(id, options = {}) { return api.delete(`/v1/journal/${id}/permanent`, options); }
 });
